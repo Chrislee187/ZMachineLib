1e45dd0ac038cf5818deb95fd82bfc5694a63695
Chris Lee <chrislee187@gmail.com>
2019-10-09 20:33:10 +0100

    Add save/restore game filename selection and overwrite check as a core feature of ZPlay

--- ZPlay/FileIo.cs
+++ ZPlay/FileIo.cs
@@ -8,24 +8,74 @@ namespace ZPlay
     {
         private readonly string _saveFileId;
 
+        private string _lastSavePath;
         public FileIo(string saveFileId)
         {
             _saveFileId = saveFileId;
         }
         public bool Save(Stream s)
         {
-            var saveFilePath = BuildSavePath();
-            s.Position = 0;
-            var fs = File.Create(saveFilePath);
-            s.CopyTo(fs);
-            fs.Close();
-            Console.WriteLine(($"Game saved to: {saveFilePath}"));
+            var saveFilePath = GetUserSavePath();
+            
+            _lastSavePath = SaveGame(s, saveFilePath);
+
             return true;
+        }
+
+        private string GetUserSavePath()
+        {
+            var saveFilePath = !string.IsNullOrEmpty(_lastSavePath) 
+                ? _lastSavePath 
+                : BuildSavePath();
+            bool confirmed;
+            do
+            {
+                var display = Path.GetFileNameWithoutExtension(saveFilePath);
+                Console.Write($"Enter save filename (press RETURN To use '{display}'): ");
+                var readLine = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(readLine))
+                {
+                    saveFilePath = Path.Combine(
+                        Path.GetDirectoryName(saveFilePath),
+                        $"{readLine}.zSave"
+                    );
+                }
+                if (File.Exists(saveFilePath))
+                {
+                    Console.Write("File already exists, overwrite Y/n?: ");
+
+                    var line = Console.ReadLine();
+                    confirmed = string.IsNullOrEmpty(line) || line.ToLower().StartsWith("y");
+                }
+                else
+                {
+                    confirmed = true;
+                }
+
+            } while (!confirmed);
+
+
+
+            return saveFilePath;
+        }
+
+        private static string SaveGame(Stream gameState, string saveFilePath)
+        {
+            gameState.Position = 0;
+            var fs = File.Create(saveFilePath);
+            gameState.CopyTo(fs);
+            fs.Close();
+            
+            Console.WriteLine(($"Game saved to: {saveFilePath}"));
+
+            return saveFilePath;
         }
 
         public Stream Restore()
         {
-            var saveFilePath = BuildSavePath();
+            var saveFilePath = GetUserSavePath();
+                
             try
             {
                 var fs = File.OpenRead(saveFilePath);
@@ -41,7 +91,7 @@ namespace ZPlay
 
         private string BuildSavePath()
         {
-            var saveFilename = $"{_saveFileId}.save";
+            var saveFilename = $"{_saveFileId}.zSave";
             var docs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             var saveDirname = "ZFileSaves";
             var savePath = Path.Combine(docs, saveDirname);
